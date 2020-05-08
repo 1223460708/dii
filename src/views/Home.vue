d010b3fd1fb6e176a99956c2f8470cf33c0a4bcc
chen <13454735035@163.com>
2020-05-08 11:29:30 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+
+    第一个模板哈哈哈哈哈
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {
+    
+  }
+};
+</script>
